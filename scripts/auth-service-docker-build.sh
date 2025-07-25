b79ade95a099851c903eea99664ed38fa696a655
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2025-07-24 20:45:43 -0400

    refactor

---
+++ scripts/auth-service-docker-build.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+
+CURDIR=$(dirname "$0")
+WORKDIR=$(cd "$CURDIR/..")
+
+cd auth-service
+docker build -t auth-service:latest .
+if [ $? -ne 0 ]; then
+    echo "Failed to build auth-service Docker image."
+    exit 1
+elif [ -z "$(docker images -q auth-service:latest)" ]; then
+    echo "auth-service Docker image is empty."
+    exit 1
+else
+    echo "auth-service Docker image built successfully."  
+fi
