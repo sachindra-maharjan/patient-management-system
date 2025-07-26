813059586f3a5a788fd88a35d08f4b10e6c75a62
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2025-07-26 11:13:48 -0400

    Tailwind configuration

--- web-app/tailwind.config.js
+++ web-app/tailwind.config.js
@@ -1,18 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,ts,css,scss}'],
-  darkMode: ['class', '[data-pc-theme="dark"]'],
-  theme: {
-    extend: {
-      colors: {
-        'theme-cardbg': 'rgb(var(--color-theme-cardbg) / <alpha-value>)',
-        'themedark-cardbg': 'rgb(var(--color-themedark-cardbg) / <alpha-value>)',
-        'theme-border': 'rgb(var(--color-theme-border) / <alpha-value>)',
-        'themedark-border': 'rgb(var(--color-themedark-border) / <alpha-value>)',
-        'theme-bodycolor': 'rgb(var(--color-theme-bodycolor) / <alpha-value>)',
-        'themedark-bodycolor': 'rgb(var(--color-themedark-bodycolor) / <alpha-value>)'
-      },
-    },
-  },
-  plugins: [],
+  content: ['./src/**/*.{html,js,ts}'],
+  darkMode: ['class', 'selector', '[data-pc-theme="dark"]'],
 };
